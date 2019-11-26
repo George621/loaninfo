64d3d681c08260b1a7234713627481d9ca699578
zhangjunshuai <zhangjunshuai@xiaomi.com>
2019-11-26 16:35:44 +0800

    waterfall component

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 // import { Index } from '../component/Index/index.jsx'
 
-import { Lott } from '../component/Lott/index.jsx'
+// import { Lott } from '../component/Lott/index.jsx'
 
 // import { Gift } from '../component/Gift/index.jsx'
-
+import {WaterFall} from '../component/WaterFall/index.jsx'
 
 
 export const App = () => {
 
   return (
     <div className="app">
-      <Lott />
+      <WaterFall />
+      {/* <Lott /> */}
       {/* <Gift></Gift> */}
     </div>
   )
