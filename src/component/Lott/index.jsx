699dc826540e9a1cfbd8d8303d22fd088cf908b6
zhangjunshuai <zhangjunshuai@xiaomi.com>
2020-01-09 14:15:09 +0800

    feat: pull trans

--- src/component/Lott/index.jsx
+++ src/component/Lott/index.jsx
@@ -5,6 +5,14 @@ export const Lott = (props) => {
   return (
     <div className="contain">
       <div className='lott'></div>
+      <div className='rainbow'>
+       <div className='r1' />
+       <div className='r2' />
+       <div className='r3' />
+       <div className='r4' />
+       <div className='r5' />
+       <div className='r6' />
+      </div>
     </div>
   )
 }
